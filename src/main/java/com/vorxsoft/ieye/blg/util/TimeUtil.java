ab2456cd9b5e63abf5eac1a22f39f1e6b718cc05
boundlesswu <boundlesswu@gmail.com>
2017-12-22 18:18:52 +0800

    123

---
+++ src/main/java/com/vorxsoft/ieye/blg/util/TimeUtil.java
@@ -0,0 +1,26 @@
+package com.vorxsoft.ieye.blg.util;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TimeUtil {
+  static public String timestmap2string(Timestamp t){
+    return "test";
+  }
+  static public Timestamp string2timestamp(String  s) {
+    //String的类型必须形如： yyyy-mm-dd hh:mm:ss[.f...] 这样的格式，中括号表示可选，否则报错！！！
+    System.out.println(s);
+    return Timestamp.valueOf(s);
+  }
+  static public  String getDateTimeNs(){
+    String msg="";
+    Date date = new Date();
+    SimpleDateFormat sdf = new SimpleDateFormat("YYYY/MM/dd HH:mm:ss.SSS");
+    msg += sdf.format(date);
+    System.out.println(msg);
+    return msg;
+  }
+
+
+}
